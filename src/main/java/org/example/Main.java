f86f6fc8881580d7ae05e2477085541f9831fccd
Ilya_Prostakov <prostocowtawer@gmail.com>
2024-07-25 19:07:22 +0700

    Add option fullStatistics

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 import org.apache.commons.cli.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
     private static void writeFile(String str, String filePath, Boolean isAppending)
@@ -67,7 +68,7 @@ public class Main {
             }
         }
         if (cmd.hasOption("statistics")) getStatistics(outputPath+"/"+filePrefix);
-        cmd.hasOption("fullStatistics");// TODO: getFullStatistics(outputPath+"/"+filePrefix);
+        if (cmd.hasOption("fullStatistics")) getFullStatistics(outputPath+"/"+filePrefix);
     }
 
     private static Options getOptions() {
@@ -91,19 +92,147 @@ public class Main {
         return options;
     }
 
-    private static void getStatistics(String fileFullPrefix) throws IOException {
+    private static void getStatistics(String fileFullPrefix) {
         System.out.println("Integers count: " + getFileLinesCount(fileFullPrefix+"integers.txt"));
         System.out.println("Floats count: " + getFileLinesCount(fileFullPrefix+"floats.txt"));
-        System.out.println("Strings count: " + getFileLinesCount(fileFullPrefix+"strings.txt"));
+        System.out.println("Strings count: " + getFileLinesCount(fileFullPrefix+"strings.txt") + "\n");
     }
 
-    private static int getFileLinesCount(String filePath) throws IOException{
-        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+    private static int getFileLinesCount(String filePath){
         int lines = 0;
-        while (reader.readLine() != null) lines++;
-        reader.close();
-        return lines;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            while (reader.readLine() != null) lines++;
+            reader.close();
+            return lines;
+        }
+        catch (IOException e) {
+            return lines;
+        }
     }
 
+    private static void getFullStatistics(String fileFullPrefix) throws IOException {
+        getIntegersStatistics(fileFullPrefix);
+        getFloatStatistics(fileFullPrefix);
+        getStringsStatistics(fileFullPrefix);
+    }
 
+    private static void getIntegersStatistics(String fileFullPrefix) throws IOException{
+        BufferedReader integerReader;
+
+        try {
+            integerReader = new BufferedReader(new FileReader(fileFullPrefix+"integers.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("INTEGERS");
+            System.out.println("COUNT: 0\n");
+            return;
+        }
+        String line = integerReader.readLine();
+        if (line == null) {
+            System.out.println("INTEGERS");
+            System.out.println("COUNT: 0\n");
+            return;
+        }
+
+        int lines = 1;
+        long max = Long.parseLong(line);
+        long min = Long.parseLong(line);
+        long sum = Long.parseLong(line);
+        double avg = Double.parseDouble(line);
+
+        while (true) {
+            line = integerReader.readLine();
+            if (line == null) break;
+            lines++;
+            long current = Long.parseLong(line);
+            if (max < current) max = current;
+            if (min > current) min = current;
+            sum += current;
+            avg = (double) sum / lines;
+        }
+
+        System.out.println("INTEGERS");
+        System.out.println("COUNT: " + lines);
+        System.out.println("SUM: " + sum);
+        System.out.println("MAX: " + max);
+        System.out.println("MIN: " + min);
+        System.out.println("AVG: " + avg + "\n");
+        integerReader.close();
+    }
+
+    private static void getFloatStatistics(String fileFullPrefix) throws IOException{
+        BufferedReader floatReader;
+
+        try {
+            floatReader = new BufferedReader(new FileReader(fileFullPrefix+"floats.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("FLOAT");
+            System.out.println("COUNT: 0");
+            return;
+        }
+        String line = floatReader.readLine();
+        if (line == null) {
+            System.out.println("FLOAT");
+            System.out.println("COUNT: 0");
+            return;
+        }
+
+        int lines = 1;
+        double max = Double.parseDouble(line);
+        double min = Double.parseDouble(line);
+        double sum = Double.parseDouble(line);
+        double avg = Double.parseDouble(line);
+
+        while (true) {
+            line = floatReader.readLine();
+            if (line == null) break;
+            lines++;
+            double current = Double.parseDouble(line);
+            if (max < current) max = current;
+            if (min > current) min = current;
+            sum += current;
+            avg = sum / lines;
+        }
+
+        System.out.println("FLOAT");
+        System.out.println("COUNT: " + lines);
+        System.out.println("SUM: " + sum);
+        System.out.println("MAX: " + max);
+        System.out.println("MIN: " + min);
+        System.out.println("AVG: " + avg + "\n");
+        floatReader.close();
+    }
+
+    private static void getStringsStatistics(String fileFullPrefix) throws IOException {
+        BufferedReader stringsReader;
+        try {
+            //Need for reading russian symbols
+            stringsReader = new BufferedReader(
+                    new InputStreamReader(new FileInputStream(fileFullPrefix+"strings.txt"), StandardCharsets.UTF_8));
+        } catch (FileNotFoundException e) {
+            System.out.println("STRINGS");
+            System.out.println("COUNT: 0");
+            return;
+        }
+        String line = stringsReader.readLine();
+        if (line == null) {
+            System.out.println("STRINGS");
+            System.out.println("COUNT: 0");
+            return;
+        }
+        int lines = 1;
+        int maxLength = line.length();
+        int minLength = line.length();
+        while (true) {
+            line = stringsReader.readLine();
+            if (line == null) break;
+            lines++;
+            if (maxLength < line.length()) maxLength = line.length();
+            if (minLength > line.length()) minLength = line.length();
+        }
+        System.out.println("STRINGS");
+        System.out.println("COUNT: " + lines);
+        System.out.println("MAX LENGTH: " + maxLength);
+        System.out.println("MIN LENGTH: " + minLength + "\n");
+        stringsReader.close();
+    }
 }
